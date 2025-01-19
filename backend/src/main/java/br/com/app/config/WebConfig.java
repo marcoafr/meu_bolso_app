ce2771bd5199ea247212795a1bf107feb2c103e1
Marco Ribeiro <marcoafr@live.com>
2025-01-19 17:32:50 -0300

    @EnableWebMvc on WebConfig

--- backend/src/main/java/br/com/app/config/WebConfig.java
+++ backend/src/main/java/br/com/app/config/WebConfig.java
@@ -3,9 +3,11 @@ package br.com.app.config;  // Pacote sugerido
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
