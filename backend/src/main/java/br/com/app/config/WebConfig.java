026267900d5d4b7224c814d494b2e77c3098fe58
Marco Ribeiro <marcoafr@live.com>
2025-01-19 17:38:55 -0300

    Tentando ajustes de permissão do CORS

--- backend/src/main/java/br/com/app/config/WebConfig.java
+++ backend/src/main/java/br/com/app/config/WebConfig.java
@@ -7,17 +7,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+// @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Configuração global para todas as rotas começando com /api
-        registry.addMapping("/api/**")
+        registry.addMapping("/**");
+                //.addMapping("/api/**")
                 //.allowedOrigins("http://localhost:5173")  // Permite requisições de localhost:5173
-                .allowedOrigins("http://103.199.186.65:5173")  // Permite requisições de 103.199.186.65:5173
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")  // Permite qualquer cabeçalho
-                .allowCredentials(true); // Permite envio de cookies, tokens, etc.
+                //.allowedOrigins("http://103.199.186.65:5173")  // Permite requisições de 103.199.186.65:5173
+                //.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                //.allowedHeaders("*");
+                //.allowCredentials(true);  // Permite qualquer cabeçalho
     }
 }
