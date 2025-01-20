7a0fd0dfbc44a4a484decbf7177d812dbd61eb0a
Marco Ribeiro <marco@statix.com.br>
2025-01-20 09:51:01 -0300

    ajuste de http para https

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:8080/api',  // Ajuste conforme o seu backend
   // baseURL: 'http://103.199.186.65:8080/api',  // Ajuste conforme o seu backend
-  baseURL: 'http://meubolso.tech/api',  // Ajuste conforme o seu backend
+  baseURL: 'https://meubolso.tech/api',  // Ajuste conforme o seu backend
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Headers': '*', // Adiciona o cabeçalho de permissão
